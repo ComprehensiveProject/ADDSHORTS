77684959ccf3a25aabfefe701e46df536a0b6ea4
YangjiwooGN <jwoo3460@gmail.com>
2024-06-03 01:23:46 +0900

    feat)ViT_test

--- src/main/reactfront/src/views/VideoSummary/index.js
+++ src/main/reactfront/src/views/VideoSummary/index.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Box, Button, Typography, Container, TextField, MenuItem, Select, FormControl, InputLabel } from '@mui/material';
+import { Box, Button, Typography, Container, MenuItem, Select, FormControl, InputLabel, Grid, IconButton, Paper, Card, CardContent } from '@mui/material';
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 import Navigation from "../Navigation";
+import { styled } from "@mui/material/styles";
+import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
 
 export default function VideoSummary() {
     const [videoFile, setVideoFile] = useState(null);
@@ -33,6 +35,61 @@ export default function VideoSummary() {
         };
         fetchUserData();
     }, [cookies.token]);
+
+    const StyledContainer = styled(Container)(({ theme }) => ({
+        marginTop: '150px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        textAlign: 'center',
+        padding: theme.spacing(4),
+        borderRadius: theme.spacing(2),
+        boxShadow: '0 4px 20px rgba(0, 0, 0, 0.1)',
+        border: '3px solid #0288d1',
+        width: '80%',
+        maxWidth: '800px',
+        backgroundColor: '#f0f4f8',
+    }));
+
+    const CustomButton = styled(Button)(({ theme }) => ({
+        backgroundColor: '#0288d1',
+        color: '#fff',
+        '&:hover': {
+            backgroundColor: '#0277bd',
+        },
+        width: '100%',
+        maxWidth: '300px',
+        padding: theme.spacing(2),
+        fontSize: '1.2rem',
+        fontWeight: 'bold',
+        marginBottom: theme.spacing(2),
+        borderRadius: '20px',
+    }));
+
+    const ChangeVideoButton = styled(Button)(({ theme }) => ({
+        backgroundColor: '#ffb300',
+        color: '#fff',
+        '&:hover': {
+            backgroundColor: '#ffa000',
+        },
+        width: '100%',
+        maxWidth: '300px',
+        padding: theme.spacing(1),
+        fontSize: '1rem',
+        fontWeight: 'bold',
+        marginBottom: theme.spacing(2),
+        borderRadius: '20px',
+    }));
+
+    const StyledVideo = styled('video')(({ theme }) => ({
+        width: '100%',
+        height: 'auto',
+        borderRadius: theme.spacing(1),
+        boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
+        maxHeight: '50vh',
+        border: '2px solid #0288d1',
+    }));
+
 
     const handleFileChange = (event) => {
         const file = event.target.files[0];
@@ -84,64 +141,110 @@ export default function VideoSummary() {
 
     return (
         <div>
-            <Navigation/>
-            <Container maxWidth="sm">
-                <Box display="flex" flexDirection="column" alignItems="center" mt={5}>
-                    <Typography variant="h5" mb={3}>Upload Video</Typography>
-                    {userId && (
-                        <TextField
-                            label="User ID"
-                            variant="outlined"
-                            fullWidth
-                            value={userId}
-                            disabled
-                            sx={{ marginBottom: 2 }}
-                        />
+            <Navigation />
+            <StyledContainer>
+                <Grid container spacing={4} alignItems="center" justifyContent="center">
+                    <Grid item xs={12} sx={{ textAlign: 'center' }}>
+                        <Typography variant="h3" sx={{ fontWeight: 'bold', color: '#0288d1', marginBottom: 4 }}>
+                            비디오 업로드
+                        </Typography>
+                    </Grid>
+                    {!videoPreview && (
+                        <Grid item xs={12}>
+                            <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+                                <input
+                                    accept="video/*"
+                                    style={{ display: 'none' }}
+                                    id="raised-button-file"
+                                    type="file"
+                                    onChange={handleFileChange}
+                                />
+                                <label htmlFor="raised-button-file" style={{ width: '100%' }}>
+                                    <CustomButton variant="contained" component="span">
+                                        비디오 선택
+                                    </CustomButton>
+                                </label>
+                            </Box>
+                        </Grid>
                     )}
-                    {videoDuration > 0 && (
-                        <FormControl fullWidth sx={{ marginBottom: 2 }}>
-                            <InputLabel id="summary-time-label">Summary Time</InputLabel>
-                            <Select
-                                labelId="summary-time-label"
-                                value={summaryTime}
-                                onChange={(e) => setSummaryTime(e.target.value)}
-                                label="Summary Time"
-                            >
-                                {generateSummaryTimeOptions().map((option) => (
-                                    <MenuItem key={option} value={option}>
-                                        {formatTime(option)}
-                                    </MenuItem>
-                                ))}
-                            </Select>
-                        </FormControl>
-                    )}
-                    <input
-                        accept="video/*"
-                        style={{ display: 'none' }}
-                        id="raised-button-file"
-                        type="file"
-                        onChange={handleFileChange}
-                    />
-                    <label htmlFor="raised-button-file">
-                        <Button variant="contained" component="span">
-                            Choose Video
-                        </Button>
-                    </label>
                     {videoPreview && (
-                        <Box mt={2}>
-                            <video width="100%" height="auto" controls>
-                                <source src={videoPreview} type="video/mp4" />
-                                Your browser does not support the video tag.
-                            </video>
-                        </Box>
+                        <>
+                            <Grid item xs={12} md={8}>
+                                <Box display="flex" justifyContent="center">
+                                    <StyledVideo controls>
+                                        <source src={videoPreview} type="video/mp4" />
+                                        브라우저가 비디오 태그를 지원하지 않습니다.
+                                    </StyledVideo>
+                                </Box>
+                            </Grid>
+                            <Grid item xs={12} md={4}>
+                                <Box display="flex" justifyContent="center" flexDirection="column" alignItems="center">
+                                    <Box display="flex" flexDirection="column" alignItems="center" sx={{ marginBottom: 3 }}>
+                                        <IconButton color="primary" onClick={() => setVideoPreview(null)}>
+                                            <ChangeCircleIcon sx={{ fontSize: 40, color: '#ffb300' }} />
+                                        </IconButton>
+                                        <Typography variant="body1" sx={{ color: '#0288d1' }}>
+                                            다른 비디오 선택
+                                        </Typography>
+                                    </Box>
+                                    {videoDuration > 0 && (
+                                        <FormControl fullWidth sx={{ marginBottom: 3, maxWidth: '300px' }}>
+                                            <InputLabel id="summary-time-label">요약 시간</InputLabel>
+                                            <Select
+                                                labelId="summary-time-label"
+                                                value={summaryTime}
+                                                onChange={(e) => setSummaryTime(e.target.value)}
+                                                label="요약 시간"
+                                            >
+                                                {generateSummaryTimeOptions().map((option) => (
+                                                    <MenuItem key={option} value={option}>
+                                                        {formatTime(option)}
+                                                    </MenuItem>
+                                                ))}
+                                            </Select>
+                                        </FormControl>
+                                    )}
+                                    <CustomButton variant="contained" onClick={handleUpload} disabled={!summaryTime}>
+                                        업로드
+                                    </CustomButton>
+                                </Box>
+                            </Grid>
+                        </>
                     )}
-                    <Box mt={2}>
-                        <Button variant="contained" color="primary" onClick={handleUpload} disabled={!summaryTime}>
-                            Upload
-                        </Button>
-                    </Box>
-                </Box>
-            </Container>
+                    {!videoPreview && (
+                        <Grid item xs={12}>
+                            <Paper elevation={3} sx={{ padding: 3, borderRadius: 2, textAlign: 'center', backgroundColor: '#e3f2fd', marginBottom: 2 }}>
+                                <Typography variant="body1" sx={{ color: '#0288d1', fontSize: '1.1rem', fontWeight: 'bold', marginBottom: 2 }}>
+                                    이 페이지에서는 비디오 파일을 업로드하여 요약할 수 있습니다. 아래 버튼을 클릭하여 비디오 파일을 선택하세요.
+                                </Typography>
+                            </Paper>
+                        </Grid>
+                    )}
+                    {!videoPreview && (
+                        <Grid item xs={12}>
+                            <Card sx={{ padding: 2, borderRadius: 2, marginBottom: 2, backgroundColor: '#e0f7fa', border: '1px solid #0288d1' }}>
+                                <CardContent>
+                                    <Typography variant="h6" sx={{ marginBottom: 2, color: '#0288d1', fontSize: '1.5rem', fontWeight: 'bold' }}>
+                                        사용 방법
+                                    </Typography>
+                                    <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+                                        <Typography variant="body1" sx={{ marginBottom: 1, color: '#0288d1', fontSize: '1.2rem', fontWeight: 'bold' }}>
+                                            1. 비디오 파일을 선택합니다.
+                                        </Typography>
+                                        <Typography variant="body1" sx={{ marginBottom: 1, color: '#0288d1', fontSize: '1.2rem', fontWeight: 'bold' }}>
+                                            2. 원하는 요약 시간을 선택합니다.
+                                        </Typography>
+                                        <Typography variant="body1" sx={{ marginBottom: 1, color: '#0288d1', fontSize: '1.2rem', fontWeight: 'bold' }}>
+                                            3. 업로드 버튼을 눌러 비디오 요약을 시작합니다.
+                                        </Typography>
+                                    </Box>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    )}
+                </Grid>
+            </StyledContainer>
         </div>
+
     );
 }
